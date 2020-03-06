c727c498be1924fbaed7d01e5dbb60ff374a4cca
bennyganapol <lebenny@gmail.com>
2020-03-07 00:54:32 +0200

    Add spaces between multi word in Challenge bar

--- src/components/ChallengeBar.js
+++ src/components/ChallengeBar.js
@@ -16,11 +16,52 @@ function ChallengeBar(props) {
         return (props.solvedIds.includes(id));
     }
 
-    return (
-        <div>
-            <div>{props.selectedChallenge.question}</div>
-            <div style={{ display: "flex", flex: "1", flexDirection: (props.horizontalDirection === 'rtl')? "row-reverse" : "row", width: props.challengeSquares.length * 60 + "px" , height:"60px" }}>
-                {props.challengeSquares.map(square =>
+    const getWidth = () => {
+        let lettersWidth = props.challengeSquares.length * 60;
+        if (props.selectedChallenge.multi && props.selectedChallenge.multi.length >= 2) {
+            lettersWidth += (props.selectedChallenge.multi.length - 1) * 30;
+        }
+        return lettersWidth;
+    }
+
+    const renderSquares = () => {
+        const squares = [];
+        let spacesIndexes = null;
+        if (props.selectedChallenge.multi && props.selectedChallenge.multi.length >= 2) {
+            spacesIndexes = [props.selectedChallenge.multi[0]]
+            for (let i = 1; i < props.selectedChallenge.multi; i++) {
+                spacesIndexes[i] = spacesIndexes[i - 1] + props.selectedChallenge.multi[i];
+            }
+        }
+
+
+        for (let i = 0; i < props.challengeSquares.length; i++) {
+            const square = props.challengeSquares[i];
+            if (spacesIndexes && spacesIndexes.includes(i)) {
+                squares.push(<div
+                    style={{ display: "flex", alignItems: "center", justifyContent: "center", flex: "0.5", }}
+                        ></ div>);
+            }
+            squares.push(
+                <Square
+                        selected={isSelected(square.id)}
+                        isSolved={isSolved(square.id)}
+                        challengeSelected={true}
+                        squareClicked={() => squareClicked(square.id)}
+                        currentLetter={props.letters[square.id]}
+                        {...square}
+                    />
+                    );
+                }
+                return squares;
+            }
+        
+        
+            return (
+        <div style={{padding : "10px"}}>
+                        <div style={{textAlign: "right", height: "35px", fontSize: "25px"}}>{props.selectedChallenge.question}</div>
+                        <div style={{ display: "flex", flex: "1", marginRight:0, marginLeft: "auto", flexDirection: (props.horizontalDirection === 'rtl') ? "row-reverse" : "row", width: getWidth() + "px", height: "60px" }}>
+                            {/* {props.challengeSquares.map(square =>
                     <Square
                         selected={isSelected(square.id)}
                         isSolved={isSolved(square.id)}
@@ -29,12 +70,13 @@ function ChallengeBar(props) {
                         currentLetter={props.letters[square.id]}
                         {...square}
                     />
-                )}
+                )} */}
+                            {renderSquares()}
 
-            </div>
-        </div>
-    );
-}
-
-
-export default ChallengeBar;
+                        </div>
+                    </div>
+                    );
+                }
+                
+                
+                export default ChallengeBar;
